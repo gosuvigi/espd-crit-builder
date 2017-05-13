cc83f548b0207842d0a8c728bda197e9eb7bcc11
gosuvigi <gosuvigi@hotmail.com>
2017-05-13 15:18:48 +0200

    display with indentation and coloring

--- react-client/src/Criterion.js
+++ react-client/src/Criterion.js
@@ -17,75 +17,92 @@ const renderField = ({input, label, type, meta: {touched, error}}) => (
 const renderSingleRequirement = (requirement, index, fields, identifier, nesting) => {
     const newIdentifier = identifier + "." + (index + 1);
     return (
-        <li key={index} className="requirement list-group-item list-group-item-success">
-            <button
-                type="button" className="btn btn-danger glyphicon glyphicon-trash"
-                title="Remove Requirement"
-                onClick={() => fields.remove(index)}
-            />
-            <Field
-                name={`${requirement}.name`}
-                type="text"
-                component={renderField}
-                label={`Requirement #${newIdentifier} nesting ${nesting}`}
-            />
-        </li>
+        <div key={index} className="requirement list-group-item list-group-item-success">
+            <div className="row">
+                <div className="col-md-3">
+                    <Field
+                        name={`${requirement}.name`}
+                        type="text"
+                        component={renderField}
+                        label={`Requirement #${newIdentifier}`}
+                    />
+                </div>
+                <div className="col-md-1">
+                    <button
+                        type="button" className="btn btn-success glyphicon glyphicon-trash"
+                        title="Remove Requirement"
+                        onClick={() => fields.remove(index)}
+                    />
+                </div>
+            </div>
+        </div>
     )
 };
 
 const renderRequirements = ({fields, meta: {error}, identifier, nesting}) => (
-    <ul className="requirements list-group">
+    <div className="requirements list-group text-left">
         {
             fields.map((requirement, index, fields) => renderSingleRequirement(requirement, index, fields, identifier, nesting))
         }
         {error && <li className="error">{error}</li>}
-        <li>
+        <div>
             <button type="button" onClick={() => fields.push()}>Add Requirement</button>
-        </li>
-    </ul>
+        </div>
+    </div>
 );
 
 const renderSingleRequirementGroup = (reqGroup, index, fields, identifier, nesting) => {
     const newIdentifier = !identifier ? (index + 1) : identifier + "." + (index + 1);
     return (
-        <li key={index} className="requirement-group list-group-item list-group-item-warning">
-            <button
-                type="button" className="btn btn-danger glyphicon glyphicon-trash"
-                title="Remove Requirement Group"
-                onClick={() => fields.remove(index)}
-            />
-            <h4>Requirement group #{newIdentifier} nesting {nesting}</h4>
+        <div key={index} className="requirement-group list-group-item list-group-item-warning text-left">
+            <div className="row">
+                <div className="col-md-3">
+                    <h4>Requirement group #{newIdentifier}</h4>
+                </div>
+                <div className="col-md-1">
+                    <button
+                        type="button" className="btn btn-danger glyphicon glyphicon-trash"
+                        title="Remove Requirement Group"
+                        onClick={() => fields.remove(index)}
+                    />
+                </div>
+            </div>
             <Field
                 name={`${reqGroup}.name`}
                 type="text"
                 component={renderField}
                 label="Group Name"
+                className="text-left"
             />
             <FieldArray name={`${reqGroup}.requirements`} component={renderRequirements} identifier={newIdentifier}
                         nesting={nesting + 1}/>
             <FieldArray name={`${reqGroup}.requirementGroups`} component={renderRequirementGroups}
                         identifier={newIdentifier} nesting={nesting + 1}/>
-        </li>
+        </div>
     )
 };
 
 const renderRequirementGroups = ({fields, meta: {error, submitFailed}, identifier, nesting}) => (
-    <ul className="requirement-groups list-group">
-        {
-            fields.map((reqGroup, index, fields) => (renderSingleRequirementGroup(reqGroup, index, fields, identifier, nesting)))
-        }
-        <li>
-            <button type="button" onClick={() => fields.push({})}>Add Requirement Group</button>
-            {submitFailed && error && <span>{error}</span>}
-        </li>
-    </ul>
+    <div className="row">
+        <div className={`col-md-11 col-md-offset-1`}>
+            <div className="requirement-groups list-group text-left">
+                {
+                    fields.map((reqGroup, index, fields) => (renderSingleRequirementGroup(reqGroup, index, fields, identifier, nesting)))
+                }
+                <div>
+                    <button type="button" onClick={() => fields.push({})}>Add Requirement Group</button>
+                    {submitFailed && error && <span>{error}</span>}
+                </div>
+            </div>
+        </div>
+    </div>
 );
 
 const Criterion = () => {
     return (
-        <div className="criterion col-md-12">
-            <div className="row pull-left">
-                <div className="col-md-12">
+        <div className="criterion col-md-12 bg-info">
+            <div className="row">
+                <div className="col-md-2 text-left">
                     <Field
                         name="name"
                         type="text"
